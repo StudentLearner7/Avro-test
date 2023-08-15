02201f5179c8b13edadf3e2dcbb2d0379a8a1551
StudentLearner7 <129011417+StudentLearner7@users.noreply.github.com>
2023-08-15 14:25:47 -0600

    Update Mm.java

--- Mm.java
+++ Mm.java
@@ -12,7 +12,11 @@ public class Main {
         cfg.setLogTemplateExceptions(false);
         cfg.setWrapUncheckedExceptions(true);
 
-        List<SimpleHash> entities = new ArrayList<>();
+        // Using DefaultObjectWrapper
+        DefaultObjectWrapper wrapper = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_31).build();
+        cfg.setObjectWrapper(wrapper);
+
+        List<Map<String, Object>> entities = new ArrayList<>();
 
         // Example: List of entity classes in EntityFolder
         String[] entityClassNames = {"Employee", "Department"}; // Add your entity class names here
@@ -20,24 +24,24 @@ public class Main {
         for (String className : entityClassNames) {
             Class<?> entityClass = Class.forName(className); // Assuming fully qualified class name
 
-            SimpleHash entity = new SimpleHash();
+            Map<String, Object> entity = new HashMap<>();
             entity.put("name", className);
 
-            List<SimpleHash> fields = new ArrayList<>();
+            List<Map<String, Object>> fields = new ArrayList<>();
             for (Field field : entityClass.getDeclaredFields()) {
-                SimpleHash fieldMap = new SimpleHash();
+                Map<String, Object> fieldMap = new HashMap<>();
                 fieldMap.put("name", field.getName());
                 fieldMap.put("type", field.getType().getSimpleName());
                 fieldMap.put("isPrimitive", isPrimitive(field.getType()));
                 fields.add(fieldMap);
             }
-            entity.put("fields", new SimpleSequence(fields));
+            entity.put("fields", fields);
             entities.add(entity);
         }
 
         // Create the data-model for ModelMapper
-        SimpleHash modelMapperData = new SimpleHash();
-        modelMapperData.put("entities", new SimpleSequence(entities));
+        Map<String, Object> modelMapperData = new HashMap<>();
+        modelMapperData.put("entities", entities);
 
         // Process ModelMapper template
         processTemplate(cfg, "ModelMapper.ftl", modelMapperData);
@@ -47,7 +51,7 @@ public class Main {
         return type.isPrimitive() || type.getName().startsWith("java.lang");
     }
 
-    private static void processTemplate(Configuration cfg, String templateName, TemplateHashModel data) throws Exception {
+    private static void processTemplate(Configuration cfg, String templateName, Map<String, Object> data) throws Exception {
         Template temp = cfg.getTemplate(templateName);
         Writer out = new OutputStreamWriter(System.out);
         temp.process(data, out);

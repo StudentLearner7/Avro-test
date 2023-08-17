5dc7ed98f9d815d5d91ebecffc6bcf5b1fafdea6
StudentLearner7 <129011417+StudentLearner7@users.noreply.github.com>
2023-08-17 14:49:53 -0600

    Update Mm.java

--- Mm.java
+++ Mm.java
@@ -1,61 +1,41 @@
-import freemarker.template.*;
-import java.io.*;
-import java.lang.reflect.Field;
-import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_31);
-        cfg.setClassForTemplateLoading(Main.class, "/");
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        cfg.setLogTemplateExceptions(false);
-        cfg.setWrapUncheckedExceptions(true);
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-        // Using DefaultObjectWrapper
-        DefaultObjectWrapper wrapper = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_31).build();
-        cfg.setObjectWrapper(wrapper);
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-        List<Map<String, Object>> entities = new ArrayList<>();
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserServiceTest {
 
-        // Example: List of entity classes in EntityFolder
-        String[] entityClassNames = {"Employee", "Department"}; // Add your entity class names here
+    @Autowired
+    private UserRepository userRepository;
 
-        for (String className : entityClassNames) {
-            Class<?> entityClass = Class.forName(className); // Assuming fully qualified class name
+    private UserService userService;
 
-            Map<String, Object> entity = new HashMap<>();
-            entity.put("name", className);
-
-            List<Map<String, Object>> fields = new ArrayList<>();
-            for (Field field : entityClass.getDeclaredFields()) {
-                Map<String, Object> fieldMap = new HashMap<>();
-                fieldMap.put("name", field.getName());
-                fieldMap.put("type", field.getType().getSimpleName());
-                fieldMap.put("isPrimitive", isPrimitive(field.getType()));
-                fields.add(fieldMap);
-            }
-            entity.put("fields", fields);
-            entities.add(entity);
-        }
-
-        // Create the data-model for ModelMapper
-        Map<String, Object> modelMapperData = new HashMap<>();
-        modelMapperData.put("entities", entities);
-
-        // Process ModelMapper template
-        processTemplate(cfg, "ModelMapper.ftl", modelMapperData);
+    @Before
+    public void setUp() {
+        userService = new UserService();
+        userService.setUserRepository(userRepository);
     }
 
-    private static boolean isPrimitive(Class<?> type) {
-        return type.isPrimitive() || type.getName().startsWith("java.lang");
-    }
+    @Test
+    public void testCreateAndFindUser() {
+        User user = new User();
+        user.setName("John Doe");
 
-    private static void processTemplate(Configuration cfg, String templateName, Map<String, Object> data) throws Exception {
-        Template temp = cfg.getTemplate(templateName);
-        Writer out = new OutputStreamWriter(System.out);
-        temp.process(data, out);
-        out.flush();
-        System.out.println(); // Line break between templates
+        User createdUser = userService.createUser(user);
+        assertNotNull(createdUser);
+        assertNotNull(createdUser.getId());
+
+        User foundUser = userService.findUserById(createdUser.getId());
+        assertEquals(createdUser.getName(), foundUser.getName());
     }
 }

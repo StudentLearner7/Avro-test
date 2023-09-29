fe07a4b4d4681a37b8f29e99c45af25ef4408cac
StudentLearner7 <129011417+StudentLearner7@users.noreply.github.com>
2023-09-29 12:28:09 -0600

    Update Mm.java

--- Mm.java
+++ Mm.java
@@ -1,13 +1,38 @@
-String input = "@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true, mappedBy = \"parentEntity\")";
+// Inside the visit method for MethodDeclaration
+@Override
+public void visit(MethodDeclaration md, Void arg) {
+    super.visit(md, arg);
 
-// Split the string by commas and spaces
-String[] parts = input.split("[,\\s]+");
+    // Check if the method has a @OneToMany annotation
+    md.getAnnotationByName("OneToMany").ifPresent(annotationExpr -> {
+        // Check if the annotation has a value for "fetch" and it's set to "FetchType.LAZY"
+        annotationExpr.ifSingleMemberAnnotationExpr(singleMember -> {
+            if (singleMember.getMemberValue().asString().equals("FetchType.LAZY")) {
+                // Modify the annotation to set fetchType to FetchType.EAGER
+                singleMember.replace(ExpressionParser.parseExpression("FetchType.EAGER"));
+                
+                // Add import statements for FetchType and CascadeType if needed
+                addImportStatement(md.findCompilationUnit(), "javax.persistence.FetchType");
+                addImportStatement(md.findCompilationUnit(), "javax.persistence.CascadeType");
+            }
+        });
 
-// Loop through the parts to find the "mappedBy" attribute
-for (int i = 0; i < parts.length; i++) {
-    if (parts[i].equals("mappedBy")) {
-        String mappedByValue = parts[i + 2].replace("\"", "");
-        System.out.println("MappedBy value: " + mappedByValue);
-        break;
-    }
+        // Add cascade type "CascadeType.ALL" if not already present
+        annotationExpr.ifNormalAnnotationExpr(normalAnnotation -> {
+            if (!normalAnnotation.getPairs().stream().anyMatch(pair ->
+                    pair.getNameAsString().equals("cascade") &&
+                    pair.getValue().asString().equals("CascadeType.ALL"))) {
+                normalAnnotation.addPair("cascade", "CascadeType.ALL");
+            }
+        });
+
+        // Add orphan removal "true" if not already present
+        annotationExpr.ifNormalAnnotationExpr(normalAnnotation -> {
+            if (!normalAnnotation.getPairs().stream().anyMatch(pair ->
+                    pair.getNameAsString().equals("orphanRemoval") &&
+                    pair.getValue().asBooleanLiteralExpr().getValue())) {
+                normalAnnotation.addPair("orphanRemoval", "true");
+            }
+        });
+    });
 }

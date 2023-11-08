16706d115224ac47b940960aba45cd93b22c191f
StudentLearner7 <129011417+StudentLearner7@users.noreply.github.com>
2023-11-08 11:16:41 -0500

    Update Mm.java

--- Mm.java
+++ Mm.java
@@ -1,38 +1,11 @@
-// Inside the visit method for MethodDeclaration
-@Override
-public void visit(MethodDeclaration md, Void arg) {
-    super.visit(md, arg);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
-    // Check if the method has a @OneToMany annotation
-    md.getAnnotationByName("OneToMany").ifPresent(annotationExpr -> {
-        // Check if the annotation has a value for "fetch" and it's set to "FetchType.LAZY"
-        annotationExpr.ifSingleMemberAnnotationExpr(singleMember -> {
-            if (singleMember.getMemberValue().asString().equals("FetchType.LAZY")) {
-                // Modify the annotation to set fetchType to FetchType.EAGER
-                singleMember.replace(ExpressionParser.parseExpression("FetchType.EAGER"));
-                
-                // Add import statements for FetchType and CascadeType if needed
-                addImportStatement(md.findCompilationUnit(), "javax.persistence.FetchType");
-                addImportStatement(md.findCompilationUnit(), "javax.persistence.CascadeType");
-            }
-        });
+public interface StoreProcExecutorRepository extends JpaRepository<Object, Long> {
+    @Procedure
+    void executeStoredProcedure1(@Param("inputParam1") String inputParam1, @Param("outputParam1") String outputParam1);
 
-        // Add cascade type "CascadeType.ALL" if not already present
-        annotationExpr.ifNormalAnnotationExpr(normalAnnotation -> {
-            if (!normalAnnotation.getPairs().stream().anyMatch(pair ->
-                    pair.getNameAsString().equals("cascade") &&
-                    pair.getValue().asString().equals("CascadeType.ALL"))) {
-                normalAnnotation.addPair("cascade", "CascadeType.ALL");
-            }
-        });
-
-        // Add orphan removal "true" if not already present
-        annotationExpr.ifNormalAnnotationExpr(normalAnnotation -> {
-            if (!normalAnnotation.getPairs().stream().anyMatch(pair ->
-                    pair.getNameAsString().equals("orphanRemoval") &&
-                    pair.getValue().asBooleanLiteralExpr().getValue())) {
-                normalAnnotation.addPair("orphanRemoval", "true");
-            }
-        });
-    });
+    @Procedure
+    void executeStoredProcedure2(@Param("inputParam2") int inputParam2, @Param("outputParam2") int outputParam2);
 }
